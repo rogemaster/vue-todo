3e25cbd7723be3a03294ae62aa72a58ce847d263
김명주 <rlaaudwn000@unionplace.co.kr>
2021-12-16 09:50:18 +0900

    중급 1일차 Todo리스트까지

---
+++ src/components/TodoList.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(todoItem, index) in todoItems" :key="index" class="shdow">
+                {{todoItem}}
+                <span class="removeBtn" @click="removeTodo(todoItem, index)">
+                    <i class="fas fa-trash-alt"></i>
+                </span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todoItems: []
+        }
+    },
+
+    created() {
+        if(localStorage.length > 0) {
+            for(var i = 0; i < localStorage.length; i++) {
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+    },
+
+    methods: {
+        removeTodo(todoItem, index) {
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0px;
+    text-align: left;
+}
+li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+.checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+}
+.checkBtnCompleted {
+    color: #b3adad;
+}
+.textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+}
+.removeBtn {
+    margin-left: auto;
+    color: #de4343;
+    cursor: pointer;
+}
+
+</style>
